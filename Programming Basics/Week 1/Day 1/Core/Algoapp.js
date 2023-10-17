b053b753f7e00c37b15053c9076bc31c51d891d7
itskayrie <souguizaineb@gmail.com>
2023-10-17 12:30:36 +0100

    commit

---
+++ Programming Basics/Week 1/Day 1/Core/Algoapp.js
@@ -0,0 +1,32 @@
+//---- level 1----//
+// Activity 1 //
+var x=5
+console.log (x)
+// 5 //
+
+// Activity 2 // 
+var x = 3
+x = x+1
+console.log (x)
+// 4 //
+
+// Activity 3 //
+var x=3
+x=x+2
+console.log (x+2)
+console.log(x)
+// 7,5 // 
+
+// Activity 4 //
+var x=5
+x=x*x
+console.log (x)
+console.log (x*2)
+//9 , 18//
+
+// Activity 5 //
+var x=5
+var y=2
+var z= x+y
+console.log(z)
+// 7 // 
