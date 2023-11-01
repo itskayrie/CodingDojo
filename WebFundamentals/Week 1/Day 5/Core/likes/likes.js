68b7e56eb169ef589785686cb4830d3db1297cb3
itskayrie <souguizaineb@gmail.com>
2023-11-01 22:36:20 +0100

    commit&push

---
+++ WebFundamentals/Week 1/Day 5/Core/likes/likes.js
@@ -0,0 +1,19 @@
+var likes=document.querySelector(".pp")
+var likes2=document.querySelector(".ppp")
+var likes3=document.querySelector(".pppp")
+// console.log(likes);
+var counter=9
+function like1() {
+    counter++
+    likes.innerText=counter+" like(s)"
+}
+var counter2=11
+function like2() {
+    counter2++
+    likes2.innerText=counter2+" like(s)"
+}
+var counter3=9
+function like3() {
+    counter3++
+    likes3.innerText=counter3+" like(s)"
+}
