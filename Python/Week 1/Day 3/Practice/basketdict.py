815516009a8c5b29a8aa7209a8fcbd4159677165
itskayrie <souguizaineb@gmail.com>
2023-11-16 19:05:11 +0100

    commit & push

--- Python/Week 1/Day 3/Practice/basketdict.py
+++ Python/Week 1/Day 3/Practice/basketdict.py
@@ -1,55 +1,85 @@
-# players = [
-#     {
+
+
+# kevin = {
 #     	"name": "Kevin Durant", 
 #     	"age":34, 
 #     	"position": "small forward", 
 #     	"team": "Brooklyn Nets"
-#     },
-#     {
+# }
+# jason = {
 #     	"name": "Jason Tatum", 
 #     	"age":24, 
 #     	"position": "small forward", 
 #     	"team": "Boston Celtics"
-#     },
-#     {
+# }
+# kyrie = {
 #     	"name": "Kyrie Irving", 
 #     	"age":32,
 #         "position": "Point Guard", 
 #     	"team": "Brooklyn Nets"
-#     },
-#     {
-#     	"name": "Damian Lillard", 
-#     	"age":33,
-#         "position": "Point Guard", 
-#     	"team": "Portland Trailblazers"
-#     },
-#     {
-#     	"name": "Joel Embiid", 
-#     	"age":32,
-#         "position": "Power Foward", 
-#     	"team": "Philidelphia 76ers"
-#     },
-#     {
-#         "name": "DeMar DeRozan",
-#         "age": 32,
-#         "position": "Shooting Guard",
-#         "team": "Chicago Bulls"
-#     }
-# ]
+# }
+    
 
 
-# class Player:
-#     def __init__(self, data)
-#         self.name = data["name"]
-#         self.age = data["age"]
-#         self.position = data["position"]
-#         self.team = data["team"]
+class Player:
+    def __init__(self, data):
+        self.name = data["name"]
+        self.age = data["age"]
+        self.position = data["position"]
+        self.team = data["team"]
+
+playerke=Player(kevin)
+playerj=Player(jason)
+playerk=Player(kyrie)
+print(playerke)
+
+players = [
+    {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Jason Tatum", 
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+    },
+    {
+    	"name": "Kyrie Irving", 
+    	"age":32,
+        "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Damian Lillard", 
+    	"age":33,
+        "position": "Point Guard", 
+    	"team": "Portland Trailblazers"
+    },
+    {
+    	"name": "Joel Embiid", 
+    	"age":32,
+        "position": "Power Foward", 
+    	"team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+new_team=[]
+for i in players:
+    new_team.append(Player(i))
+print(new_team)
 
 
 
 
-# user=Player()
-# print(user)
 
 
 
