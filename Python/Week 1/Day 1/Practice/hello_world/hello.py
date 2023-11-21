adb136b809cb7453d2359da2939a059f127b230c
itskayrie <souguizaineb@gmail.com>
2023-11-21 03:57:14 +0100

    commit & push

---
+++ Python/Week 1/Day 1/Practice/hello_world/hello.py
@@ -0,0 +1,12 @@
+# def hello():
+#     # print("hello_world")
+# hello()
+name="kayrie"
+print ("hello",name,"!")
+
+name="42"
+print("hello",name,"!")
+
+favourite_shoes1="nike"
+favouriteshoes2="diadora"
+print(f"i wear {favourite_shoes1} and {favouriteshoes2}")
