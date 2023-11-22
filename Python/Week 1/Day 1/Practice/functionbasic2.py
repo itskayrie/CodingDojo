5bfc356d8ab4a187188aef4e7caaa8f0bfe7ca3e
itskayrie <souguizaineb@gmail.com>
2023-11-22 02:01:09 +0100

    c&p

---
+++ Python/Week 1/Day 1/Practice/functionbasic2.py
@@ -0,0 +1,41 @@
+# 1--------
+def countdown(num):
+    output=[]
+    for i in range(num,-1,-1)
+    output.append(i)
+    return output
+
+# 2---------
+def printt(list):
+    print(list[0])
+    return list[i]
+print(printt([1,2]))
+
+# 3--------
+def summ(list):
+    return lis[0]+len(list)
+print(summ([1,2,3]))
+
+
+# 4---------
+if len(list)<2 :
+    return False
+output=[0]
+for i in range(0,len(list)):
+    if(list[i]>list[1]):
+        output.append(list[i])
+        print(len(output))
+        return output
+    
+# 5----------
+def len_val(size,value):
+    output=[]
+    for i in range (0,size)
+    output.append(value)
+    return output
+
+print(len_val(8,9))
+
+
+
+    
