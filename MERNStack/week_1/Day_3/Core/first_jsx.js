5f04adee432dd1af2f9e8afe85f21be57d05774d
itskayrie <souguizaineb@gmail.com>
2023-12-20 22:25:28 +0100

    commit

---
+++ MERNStack/week_1/Day_3/Core/first_jsx.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Displayperson from "./components/Displayperson";
+
+function App() {
+  // const person={
+  //   name:"joe",
+  //   age:45,
+  //   favFood:"pizza",
+  // };
+return (
+    <div className="App">
+    <h1>hello dojo !</h1>
+    <h2>things i need to do :</h2>
+    <ol>
+        <li>learn react </li>
+        <li>climb mt everest</li>
+        <li>run a marathon</li>
+        <li>feed the dogs</li>
+    </ol>
+    </div>
+);
+}
+
+export default App;
