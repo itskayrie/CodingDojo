6d1014b6e2888ebe62af4668f132d02f570daf2e
itskayrie <souguizaineb@gmail.com>
2023-12-22 02:04:21 +0100

    commit

---
+++ MERNStack/week_1/Day_3/Core/first_jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello Dojo !!</h1>
+      <h2>Things i need to do </h2>
+      <ol>
+        <ul>Learn react</ul>
+        <ul>climb mt_everest</ul>
+        <ul>run a marathon </ul>
+        <ul>feed the dogs</ul>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
